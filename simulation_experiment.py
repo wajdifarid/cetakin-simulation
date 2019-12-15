0ad261352a98832bd7c69de41d188fe5a37f720a
wajdifarid <faridwajdi05@gmail.com>
2019-12-15 19:05:04 +0700

    Refactor experiment simulation

--- simulation_experiment.py
+++ simulation_experiment.py
@@ -21,27 +21,77 @@ def average(array):
     return sum(array) / len(array)
 
 
-def run_simulation(number_of_simulation):
+def run_cetakin_simulation(number_of_simulation):
     service_rates = []
     average_queue_times = []
     customer_serveds = []
-    server_utilizations = []
+    online_printer_utilizations = []
+    offline_printer_utilizations = []
+    computer_utilizations = []
 
     for i in range(number_of_simulation):
         env = simpy.Environment()
 
-        printers = (
-            [MonitoredResource(env, capacity=HYBRID_PRINTER_CAPACITY)]
-            if IS_PRINTER_HYBRID
-            else [
-                MonitoredResource(env, capacity=ONLINE_PRINTER_CAPACITY),
-                MonitoredResource(env, capacity=OFFLINE_PRINTER_CAPACITY),
-            ]
-        )
         computer = MonitoredResource(env, capacity=COMPUTER_CAPACITY)
         offline_current_time = 0
-        offline_customer_number = 0
-        offline_printer = printers[0] if IS_PRINTER_HYBRID else printers[1]
+        offline_customer_number = 1
+        offline_printer = MonitoredResource(env, capacity=OFFLINE_PRINTER_CAPACITY)
+        while offline_current_time < OPEN_DURATION_MINUTES:
+            OfflineCustomer(
+                env,
+                "Offline Customer %d" % offline_customer_number,
+                offline_printer,
+                offline_current_time,
+                computer,
+            )
+            offline_current_time += numpy.random.exponential(
+                OFFLINE_CUSTOMER_INTERARRIVAL_SCALE_MINUTES
+            )
+            offline_customer_number += 1
+
+        online_current_time = 0
+        online_customer_number = 1
+        online_printer = MonitoredResource(env, capacity=ONLINE_PRINTER_CAPACITY)
+        while online_current_time < OPEN_DURATION_MINUTES:
+            OnlineCustomer(env, "Online Customer %d" % online_customer_number, online_printer, online_current_time)
+            online_current_time += numpy.random.exponential(
+                ONLINE_CUSTOMER_INTERARRIVAL_SCALE_MINUTES
+            )
+            online_customer_number += 1
+
+        env.run()
+        customer_served = offline_customer_number + online_customer_number
+        service_rate = customer_served / OPEN_DURATION_MINUTES
+
+        service_rates.append(service_rate)
+        customer_serveds.append(customer_served)
+        online_printer_utilizations.append(online_printer.utilization)
+        offline_printer_utilizations.append(offline_printer.utilization)
+        computer_utilizations.append(computer.utilization)
+
+    return (
+        average(service_rates),
+        average(customer_serveds),
+        average(online_printer_utilizations),
+        average(offline_printer_utilizations),
+        average(computer_utilizations)
+    )
+
+def run_hybrid_simulation(number_of_simulation):
+    service_rates = []
+    average_queue_times = []
+    customer_serveds = []
+    printer_utilizations = []
+    computer_utilizations = []
+
+    for i in range(number_of_simulation):
+        env = simpy.Environment()
+
+        printers = MonitoredResource(env, capacity=HYBRID_PRINTER_CAPACITY)
+        computer = MonitoredResource(env, capacity=COMPUTER_CAPACITY)
+        offline_current_time = 0
+        offline_customer_number = 1
+        offline_printer = printers
         while offline_current_time < OPEN_DURATION_MINUTES:
             OfflineCustomer(
                 env,
@@ -55,36 +105,52 @@ def run_simulation(number_of_simulation):
             )
             offline_customer_number += 1
 
-        current_time = 0
-        customer_number = 0
-        online_printer = printers[0]
-        while current_time < OPEN_DURATION_MINUTES:
-            OnlineCustomer(env, "Customer %d" % customer_number, online_printer, current_time)
-            current_time += numpy.random.exponential(
+        online_current_time = 0
+        online_customer_number = 1
+        online_printer = printers
+        while online_current_time < OPEN_DURATION_MINUTES:
+            OnlineCustomer(env, "Customer %d" % online_customer_number, online_printer, online_current_time)
+            online_current_time += numpy.random.exponential(
                 ONLINE_CUSTOMER_INTERARRIVAL_SCALE_MINUTES
             )
-            customer_number += 1
+            online_customer_number += 1
 
         env.run()
-        customer_served = offline_customer_number
+        customer_served = offline_customer_number + online_customer_number
         service_rate = customer_served / OPEN_DURATION_MINUTES
+        
         service_rates.append(service_rate)
         customer_serveds.append(customer_served)
-        server_utilizations.append(printers[0].utilization)
+        printer_utilizations.append(printers.utilization)
+        computer_utilizations.append(computer.utilization)
 
     return (
         average(service_rates),
         average(customer_serveds),
-        average(server_utilizations),
+        average(printer_utilizations),
+        average(computer_utilizations)
     )
 
 
 if __name__ == "__main__":
-    (service_rate, customer_served, server_utilization,) = run_simulation(
-        SIMULATION_NUMBER
-    )
+    if IS_PRINTER_HYBRID:
+        (service_rate, customer_served, printer_utilization, computer_utilization) = run_hybrid_simulation(
+            SIMULATION_NUMBER
+        )
+        with open("result.txt", "a") as f:
+            f.write("Service Rate: " + str(service_rate) + "\n")
+            f.write("Jumlah Customer terlayani : " + str(customer_served) + "\n")
+            f.write("Utilisasi Printer : " + str(printer_utilization) + "\n")
+            f.write("Utilisasi Komputer : " + str(computer_utilization) + "\n")
 
-    with open("result.txt", "a") as f:
-        f.write("Service Rate: " + str(service_rate) + "\n")
-        f.write("Jumlah Customerterlayani : " + str(customer_served) + "\n")
-        f.write("Utilisasi Server : " + str(server_utilization) + "\n")
+    else:
+        (service_rate, customer_served, online_printer_utilization, offline_printer_utilization, computer_utilization) = run_cetakin_simulation(
+            SIMULATION_NUMBER
+        )
+
+        with open("result.txt", "a") as f:
+            f.write("Service Rate: " + str(service_rate) + "\n")
+            f.write("Jumlah Customer terlayani : " + str(customer_served) + "\n")
+            f.write("Utilisasi Printer Online : " + str(online_printer_utilization) + "\n")
+            f.write("Utilisasi Printer Offline : " + str(offline_printer_utilization) + "\n")
+            f.write("Utilisasi Komputer : " + str(online_printer_utilization) + "\n")
